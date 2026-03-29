74ff291e4e2885d7bbb75e4ab0eca386ea5ec43c
treetopgrowthstrategy <123456789+treetopgrowthstrategy@users.noreply.github.com>
2026-03-28 20:26:16 -0500

    rebuild to match design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://treetopgrowthstrategy.com',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap({ serialize: (item) => item })],
   output: 'static',
 });
